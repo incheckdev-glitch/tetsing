bec06040ef3d673e010b05e248403dbc956494ff
incheckdev-glitch <incheckdev@gmail.com>
2026-03-05 13:12:21 +0200

    Delete assets/js directory

--- assets/js/config.js
+++
@@ -1,5 +0,0 @@
-// Example: "https://script.google.com/macros/s/XXXXX/exec"
-window.APP_CONFIG = {
-  WEBAPP_EXEC_URL: "https://script.google.com/macros/s/AKfycbxViUPX1ecYBNOWPUZPa8WjkXMJzhj66Y9dqUPenkhwBG6PGd4hYgl3y_o8DRka9Oz-/exec",
-  DEFAULT_URGENT_TOP_N: 12,
-};
